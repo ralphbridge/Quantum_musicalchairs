f171a5d6cfffcb42d06c3079f8712736f6635b2f
ralphbridge <ralphqm7@gmail.com>
2025-05-20 17:15:20 -0500

    Went back one step to understand why euler method is not working properly

--- coulomb.cu
+++ coulomb.cu
@@ -279,7 +279,7 @@ void onDevice(double *r_h,double *theta_h,double *phi_h,double *p_h,double *thet
 	//E field GPU to CPU migration(for debugging only)
 	cudaMemcpy(E_h,E,3*N*sizeof(double),cudaMemcpyDeviceToHost);
 
-	paths_euler<<<blocks,TPB>>>(r,p,E); // 
+	//paths_euler<<<blocks,TPB>>>(r,p,E); // 
 
 	cudaFree(devStates_r);
 	cudaFree(r_d);
