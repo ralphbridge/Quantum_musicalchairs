2980ed2cd1aa66676b319425d51f6286c704d1eb
ralphbridge <ralphqm7@gmail.com>
2025-06-03 23:42:22 -0500

    Added matrix computation from electron positions at the detector level for Mahin

--- coulomb_repulsion.m
+++ coulomb_repulsion.m
@@ -1,6 +1,5 @@
 data1=readmatrix("trajectoriesMay22_12_26.txt");
 
-N=100;
 xi=zeros(length(data1)/2,1);
 xf=zeros(length(data1)/2,1);
 yi=zeros(length(data1)/2,1);
@@ -23,3 +22,26 @@ hold on
 plot(xf*1e3,yf*1e3,'.')
 xlabel('x mm','FontSize',16)
 ylabel('y mm','FontSize',16)
+axis([-5 5 -5 5])
+grid on
+
+L=10e-3; % Detector size (assume it's a square detector)
+N=400; % Number of detectors (make sure it's a square)
+l=L/N; % Make sure this returns an integer
+
+S=linspace(-L/2,L/2,sqrt(N)); % Side of the detector (no need to create a meshgrid)
+M=zeros(length(S));
+
+for i=1:sqrt(N)-1
+    for j=1:sqrt(N)-1
+        for m=1:length(xf)
+            if S(i)<=xf(m) && xf(m)<S(i+1) && S(j)<=yf(m) && yf(m)<S(j+1)
+                M(i,j)=1;
+            end
+        end
+    end
+end
+
+figure
+[xx, yy] = meshgrid(1:size(M,1),1:size(M,2));
+plot3(xx,yy,M,'*')
