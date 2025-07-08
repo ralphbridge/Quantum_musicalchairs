20801be56df5c8311829c512b458199f4db5ed6d
ralphbridge <ralphqm7@gmail.com>
2025-07-08 15:31:04 -0500

    Fixed typo in printf

--- coulomb.cu
+++ coulomb.cu
@@ -519,10 +519,10 @@ __global__ void paths_euler(double *r,double *p,double *E){
 				printf("Ex=%f for particle %d\n",rtip*Vtip*r[3*idx]/pow(pow(r[3*idx],2.0)+pow(r[3*idx+1],2.0)+pow(r[3*idx+2],2.0),3.0/2.0));
 				__syncthreads();
 				//printf("Ey=%f for particle %d\n",Vtip*r[3*idx+1]*(1.0/pow(R1,3.0)-1.0/pow(R2,3.0))/(1.0/rtip-1.0/(2.0*zdet)),idx);
-				printf(rtip*Vtip*r[3*idx+1]/pow(pow(r[3*idx],2.0)+pow(r[3*idx+1],2.0)+pow(r[3*idx+2],2.0),3.0/2.0));
+				printf("Ey=%f for particle %d\n",rtip*Vtip*r[3*idx+1]/pow(pow(r[3*idx],2.0)+pow(r[3*idx+1],2.0)+pow(r[3*idx+2],2.0),3.0/2.0));
 				__syncthreads();
 				//printf("Ez=%f for particle %d\n",Vtip*((r[3*idx+2]-2.0*zdet)/pow(R1,3.0)-r[3*idx+2]/pow(R2,3.0))/(1.0/rtip-1.0/(2.0*zdet)),idx);
-				printf(rtip*Vtip*r[3*idx+2]/pow(pow(r[3*idx],2.0)+pow(r[3*idx+1],2.0)+pow(r[3*idx+2],2.0),3.0/2.0));
+				printf("Ez=%f for particle %d\n",rtip*Vtip*r[3*idx+2]/pow(pow(r[3*idx],2.0)+pow(r[3*idx+1],2.0)+pow(r[3*idx+2],2.0),3.0/2.0));
 			}
 
 			++iter;
