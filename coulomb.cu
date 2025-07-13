4a44196a806301bec7e96ffe7d9555724cc5ff2d
ralphbridge <ralphqm7@gmail.com>
2025-07-13 14:37:01 -0500

    Fixed typo

--- coulomb.cu
+++ coulomb.cu
@@ -325,7 +325,7 @@ void onDevice(double *r_h,double *theta_h,double *phi_h,double *p_h,double *thet
 	if(myfile.is_open()){
 		for(unsigned i=0;i<results.size()-1;i=i+10){
 			if(results[i]+results[i+1]!=0){
-				myfile << std::scientific << results[i] << ',' << results[i+1] << ',' << results[i+2]  << ',' << results[i+3]  << ',' << results[i+4]  << ',' << results[i+5] << ',' results[i+6] << ',' results[i+7] << ',' results[i+8] << ',' << std::defaultfloat << static_cast<int>(results[i+9]) << '\n';
+				myfile << std::scientific << results[i] << ',' << results[i+1] << ',' << results[i+2]  << ',' << results[i+3]  << ',' << results[i+4]  << ',' << results[i+5] << ',' << results[i+6] << ',' << results[i+7] << ',' << results[i+8] << ',' << std::defaultfloat << static_cast<int>(results[i+6]) << '\n';
 			}
 		}
 		std::cout << '\n';
