8be3d8568a0827053950e641b6531ff4c5c46aca
ralphbridge <ralphqm7@gmail.com>
2025-07-15 19:16:04 -0500

    Found another dumb mistake

--- coulomb.cu
+++ coulomb.cu
@@ -313,7 +313,7 @@ void onDevice(double *r_h,double *theta_h,double *phi_h,double *p_h,double *thet
 
 	paths_euler<<<blocks,TPB>>>(r,p,E);
 
-	int dsizes=12*2*N;
+	int dsizes=12*steps*N;
 
 	std::vector<double> results(dsizes);
 	cudaMemcpyFromSymbol(&(results[0]),dev_traj,dsizes*sizeof(double));
