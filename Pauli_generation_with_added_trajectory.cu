e8bdd53389ebd88486602da785f2692e0d88d5a8
Raul Puente <rpuente2@huskers.unl.edu>
2026-08-16 19:27:25 -0500

    Done Arjun's request

--- Pauli_generation_with_added_trajectory.cu
+++ Pauli_generation_with_added_trajectory.cu
@@ -22,7 +22,7 @@ Euler:	31 4-Byte registers, 24 Bytes of shared memory per thread. 1080Ti => 100.
 
 #define traj 0 // 1 for tracking trajectories, 0 for not tracking them
 
-#define N 1000 // Number of electrons
+#define N 10 // Number of electrons
 
 #define steps 100000 // Maximum allowed number of steps to kill simulation
 
